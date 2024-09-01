e8370d13c095ac5afd43f2deea0e45eeeb7d3c3c
Abhinav Chadaga <abhinav.chadaga@utexas.edu>
2024-09-01 17:20:07 -0500

    lsp + avante basic setup

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- neotree setup
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
+
+-- basic editor settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- include lazy.nvim
+require("config.lazy")
