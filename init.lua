8b6d817a46c62a78ce2133f011e632ac0a2b3b14
abhinavchadaga <abhinav.chadaga@utexas.edu>
2025-01-24 01:45:08 -0600

    initial commit with keymaps, options, colorscheme, and treesitter

---
+++ init.lua
@@ -0,0 +1,170 @@
+---------------
+--- KEYMAPS ---
+---------------
+
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- for copy paste
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copy to system clipboard", noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste to system clipboard", noremap = true, silent = true })
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlight" })
+
+-- increment numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "increment number" })
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+---------------
+--- OPTIONS ---
+---------------
+
+-- disable netrw for nvim-tree
+-- vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
+
+local opt = vim.opt
+local g = vim.g
+
+-- LH line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 space tabs
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting a new one
+
+-- turn off line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- use case-sensitive search when search term is mixed case
+
+opt.cursorline = true
+
+-- some color settings
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+local function is_ssh()
+  return vim.env.SSH_CLIENT ~= nil
+end
+
+-- clipboard support for orbstack and ssh
+local kernel_name = vim.fn.system("uname -r"):lower()
+if string.find(kernel_name, "orbstack") then
+  vim.g.clipboard = {
+    name = "pbcopy",
+    copy = {
+      ["+"] = "pbcopy",
+      ["*"] = "pbcopy",
+    },
+    paste = {
+      ["+"] = "pbpaste",
+      ["*"] = "pbpaste",
+    },
+    cache_enabled = 0,
+  }
+elseif is_ssh() then
+  g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+else
+  opt.clipboard = ""
+end
+
+-- window settings
+opt.splitright = true
+opt.splitbelow = true
+
+-----------------
+--- LAZY.NVIM ---
+-----------------
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+
+    -----------------
+    -- COLORSCHEME --
+    -----------------
+
+    {
+      "bluz71/vim-nightfly-colors",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        vim.cmd([[colorscheme nightfly]])
+      end,
+    },
+
+    ----------------
+    -- TREESITTER --
+    ----------------
+
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = ":TSUpdate",
+      config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+          ensure_installed = { "c", "cpp", "python", "javascript", "typescript", "lua", "vim", "markdown", "css", "html", "markdown_inline" },
+          sync_install = false,
+          auto_install = true,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+      end
+    },
+  },
+  install = { colorscheme = { "habamax" } },
+  checker = { enabled = true },
+})
+
