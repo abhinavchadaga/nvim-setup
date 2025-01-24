15addcd40d093a9d2d20c529e4bd93cd6447f054
abhinavchadaga <abhinav.chadaga@utexas.edu>
2025-01-24 02:23:39 -0600

    add telescope plugin

--- init.lua
+++ init.lua
@@ -165,6 +165,33 @@ require("lazy").setup({
         ts_install.compilers = { "cc", "c++" }
       end
     },
+
+    ---------------
+    -- TELESCOPE --
+    ---------------
+    {
+      'nvim-telescope/telescope.nvim',
+      branch = "0.1.x",
+      dependencies = { 
+        "nvim-lua/plenary.nvim", 
+        { "nvim-tree/nvim-web-devicons", opts = {} },
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+      },
+      config = function()
+        -- load telescope extensions
+        local telescope = require("telescope")
+        telescope.load_extension("fzf")
+        telescope.load_extension("ui-select")
+
+        -- keymaps
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      end
+    }
   },
   install = { colorscheme = { "habamax" } },
   checker = { enabled = true },
